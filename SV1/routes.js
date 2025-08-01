e448c028adaf2ddde2f6be604d5a85a354265236
keshavsoft <nknnkumar@yahoo.com>
2025-08-01 10:52:40 +0530

    v1 latest

--- SV1/routes.js
+++ SV1/routes.js
@@ -3,11 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 import { router as routerFromJournalsTable } from "./JournalsTable/routes.js";
-import { router as routerFromReceiptsTable } from "./ReceiptsTable/routes.js";
-import { router as routerFromAccountsTable } from "./AccountsTable/routes.js";
 
 router.use("/JournalsTable", routerFromJournalsTable);
-router.use("/ReceiptsTable", routerFromReceiptsTable);
-router.use("/AccountsTable", routerFromAccountsTable);
 
 export { router };
--- V1/JournalsTable/Read/routes.js
+++ V1/JournalsTable/Read/routes.js
@@ -1,5 +1,4 @@
 import express from 'express';
-
 const router = express.Router();
 
 import { router as routerFromAsIs } from "./1.AsIs/routes.js";
@@ -8,18 +7,15 @@ import { router as routerFromSelColsAsArray } from "./3.SelColsAsArray/routes.js
 import { router as routerFromSelColumns } from "./4.SelColumns/routes.js";
 import { router as routerFromSingleColumn } from "./5.SingleColumn/routes.js";
 import { router as routerFromSetSingleColumn } from "./6.SetSingleColumn/routes.js";
+import { router as routerFromKeyCount } from "./7.KeyCount/routes.js";
 
-import { router as routerFromGroupSingleColLength } from "../GroupBy/SingleColLength/routes.js";
-import { router as routerFromGroupSingleColumn } from "../GroupBy/1.SingleColumn/routes.js";
+router.use("/AsIs", routerFromAsIs);//return all original data
+router.use("/RowDataWithPk", routerFromRowDataWithPk);//return a row using pk
+router.use("/SelColsAsArray", routerFromSelColsAsArray);//returns selected col as an array of values
+router.use("/SelColumns", routerFromSelColumns);// returns selected col as an object
+router.use("/SingleColumn", routerFromSingleColumn);//returns all values from a col
+router.use("/SetSingleColumn", routerFromSetSingleColumn);// returns unique values from a single col
+router.use("/KeyCount", routerFromKeyCount);//returns count of each keys/value in a col
 
-router.use("/AsIs", routerFromAsIs);
-router.use("/RowDataWithPk", routerFromRowDataWithPk);
-router.use("/SelColsAsArray", routerFromSelColsAsArray);
-router.use("/SelColumns", routerFromSelColumns);
-router.use("/SingleColumn", routerFromSingleColumn);
-router.use("/SetSingleColumn", routerFromSetSingleColumn);
-
-router.use("/GroupSingleColLength", routerFromGroupSingleColLength);
-router.use("/GroupSingleColumn", routerFromGroupSingleColumn);
 
 export { router };
